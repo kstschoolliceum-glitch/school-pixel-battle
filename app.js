5de09e1ee997f1799f9332b1ca81d646a83057c9
kstschoolliceum-glitch <kstschoolliceum@gmail.com>
2026-09-01 14:55:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ let pixelCount = 0;
  * Пока cooldown делаем локальным.
  * Когда подключим Supabase, проверка будет серверной.
  */
-const COOLDOWN_SECONDS = 20;
+const COOLDOWN_SECONDS = 10;
 
 let cooldownRemaining = 0;
 let cooldownTimer = null;
